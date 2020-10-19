2907012ada358fb49e2c6fabd042cfe8f6916b62
Manny <mannysalazar@quickenloans.com>
2020-10-19 13:09:51 -0400

    Add project files.

---
+++ GrantsDiner/Services/IDAL.cs
@@ -0,0 +1,21 @@
+﻿using GrantsDiner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrantsDiner.Services
+{
+    public interface IDAL
+    {
+        IEnumerable<string> GetMenuCategories();
+        MenuItem GetMenuItemDetail(int id);
+        IEnumerable<MenuItem> GetMenuItems();
+        IEnumerable<MenuItem> GetMenuItemsByCategory(string cat);
+        IEnumerable<JoinedItem> GetCart(int id);
+        int AddToCart(ShoppingCart cartItem);
+        int DeleteFromCart(int id);
+        int UpdateInCart(ShoppingCart item);
+
+    }
+}
